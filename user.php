15547bca427db5d0d2db8ffc21e43840645a234f
Erastenko <serge.erastenko@mail.ru>
2022-07-20 16:02:05 +0300

    two

---
+++ user.php
@@ -0,0 +1,19 @@
+<?php
+class User
+{
+public  $login, $password, $email,$name;
+
+function __construct($login, $password, $email,$name)
+{
+    $this->login = $login;
+    $this->password = $password;
+    $this->email = $email;
+    $this->name = $name; 
+}
+
+}
+
+
+
+
+?>
