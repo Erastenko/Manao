2620f21540dbf1477a2fc0659a5733183d967d6f
Erastenko <serge.erastenko@mail.ru>
2022-08-04 12:53:41 +0300

    slash fix

--- links.php
+++ links.php
@@ -1,6 +1,6 @@
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<script src="JS/jquery-3.6.0.min.js" async="async"></script>
-<script src="JS/action.js" async="async"></script>
+<link href="css\style.css" rel="stylesheet" type="text/css">
+<script src="JS\jquery-3.6.0.min.js" async="async"></script>
+<script src="JS\action.js" async="async"></script>
 
 <noscript>
     <meta http-equiv="refresh" content="0; url=noscript.html">
